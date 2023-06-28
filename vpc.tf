4fc36dba8102b6a2567818bfe82c7703bde846c0
Renukaratnam <renukabendadi@gmail.com>
2023-06-28 01:56:52 +0000

    public subnet created

--- vpc.tf
+++ vpc.tf
@@ -9,3 +9,28 @@ resource "aws_vpc" "big-vpc" {
   }
 
 }
+
+# public subnet -A
+resource "aws_subnet" "big-pub-sn-A" {
+  vpc_id     = aws_vpc.big-vpc.id
+  cidr_block = "10.0.0.0/20"
+ availability_zone ="ap-south-1a"
+ map_public_ip_on_launch = "true"
+  tags = {
+    Name = "big-pub-sn-A"
+  }
+}
+
+
+
+# public subnet -B
+
+resource "aws_subnet" "big-pub-sn-B" {
+  vpc_id     = aws_vpc.big-vpc.id
+  cidr_block = "10.0.16.0/20"
+ availability_zone ="ap-south-1b"
+ map_public_ip_on_launch = "true"
+  tags = {
+    Name = "big-pub-sn-B"
+  }
+}
